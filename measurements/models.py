9f80abd88b9252c89296a7c0149d768a078f865a
Juan Lovera <juanma.lovera@hotmail.com>
2022-01-16 18:32:29 -0500

    Setup Django project and create Measurments model

---
+++ measurements/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Measurement(models.Model):
+    weight = models.FloatField()
+    fat_percentage = models.FloatField()
+    muscle_percentage = models.FloatField()
+    calories = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
